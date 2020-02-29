5b2d8018c1a8f9d45f8f09b6b94af279d13c6b90
Noah Sissons <constro49@gmail.com>
2020-02-29 17:35:19 -0500

    Merge branch 'master' of C:\Users\const\WebstormProjects\SchedulingSystemAgile with conflicts.

--- src/js/CreateGroup/creategroup.js
+++ src/js/CreateGroup/creategroup.js
@@ -1,15 +1,33 @@
 
-const form = document.querySelector('#group')
+class createGroup {
 
+  constructor() {
+
+  //const db = firebase.firestore();
+
+}
+//db.collection('groups').get().then((snapshot) => {
+
+}
+const form = document.querySelector('#group');
+if (!sessionStorage.uid) {
+  sessionStorage.uid = "hrYlfJgZzOhhEC1dNrC1DFCziwN2";
+}
   // saving data
 console.log("HERE");
   form.addEventListener('submit', (e) => {
     // prevents page refresh
     e.preventDefault();
-    firebase.firestore().collection('groups').add({
+    db.collection('groups').add({
       name : form.gname.value,
-      description : form.gdescrip.value
-                                })
+      description : form.gdescrip.value,
+      owner : db.collection("users").doc(sessionStorage.uid),
+    }).then((doc)=> {
+      db.collection('groups/' + doc.id + '/members').add({
+        role: "owner",
+        user: db.collection("users").doc(sessionStorage.uid),
+      });
+    });
       // clears fields after value is entered
       console.log("HERE");
       form.gname.value;

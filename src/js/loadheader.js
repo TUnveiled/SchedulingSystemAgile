b84fa85e9fdec2650ae7995e81607eb471cd1afa
Noah Sissons <constro49@gmail.com>
2020-03-10 17:48:03 -0400

    fixed an issue where the back button didnt work

--- src/js/loadheader.js
+++ src/js/loadheader.js
@@ -20,4 +20,18 @@ headrequest.onloadend = function() {
 
 headrequest.send();
 
-// tack scripts on here
+function LogOut() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        sessionStorage.token = null;
+
+        // The signed-in user info.
+        sessionStorage.email = null;
+        sessionStorage.name = null;
+        sessionStorage.uid = null;
+        window.location.replace("Login.html");
+
+    }).catch(function(error) {
+        alert(error)
+    });
+}
